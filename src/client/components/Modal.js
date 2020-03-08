bde6ac452fd2d6ec86093a35ff573cdfa372a0d5
Rafael Arcieri <rafaelarcieri@gmail.com>
2020-03-07 18:09:51 -0800

    fix autofocus on Modal component

--- src/client/components/Modal.js
+++ src/client/components/Modal.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class Modal extends Component {
 
+	componentDidUpdate() {
+		if(this.props.show) {
+			this.closeButton.focus();
+		}
+	}
+
 	render() {
 		
 		return (
@@ -15,7 +21,11 @@ class Modal extends Component {
 							{this.props.body}
 						</div>
 						<div className="modal-footer">
-							<button onClick={this.props.hideModal} className="btn btn-primary">OK</button>
+							<button 
+								onClick={this.props.hideModal} 
+								className="btn btn-primary"
+								ref={(button) => { this.closeButton = button; }}
+							>OK</button>
 						</div>
 					</div>
 				</div>
